f1735d644df034efbcae443bdc36783cc33f0b6c
Bernardo Belchior <bernardo.belchior1@gmail.com>
2017-08-24 11:36:50 +0100

    Now every dir inside src is linked. Added kcompile.

--- install.sh
+++ install.sh
@@ -3,23 +3,32 @@
 DIR_PATH=$(realpath "$1")
 CUR_PATH=$(realpath .)
 
-echo "Searching in $DIR_PATH"
+#find "$DIR_PATH/kernel" -maxdepth 1 -type d &> /dev/null
+#if [ $? -ne 0 ]; then
+#	echo "Path has no directory 'kernel'. Exiting.."
+#	exit 1
+#fi
 
-find "$DIR_PATH/kernel" -maxdepth 1 -type d &> /dev/null
-if [ $? -ne 0 ]; then
-	echo "Path has no directory 'kernel'. Exiting.."
-	exit 1
-fi
+cp "kcompile.sh" "$DIR_PATH/.."
 
-NEW_FILES=$(find "./kernel" -type f | cut -b 10-)
+# len(./src/kernel) = 14
+NEW_FILES=$(find "./src" -type f | cut -b 7-)
+NEW_DIRS=$(find "./src" -type d | cut -b 7-)
 
+# Creates missing directories
+while read -r dirname; do
+	echo "Creating dir $DIR_PATH/$dirname"
+	mkdir -p "$DIR_PATH/$dirname"
+done <<< "$NEW_DIRS"
+
+# Links files from repo to DIR_PATH
 while read -r filename; do
-	new_filename=$(echo "$DIR_PATH/kernel/$filename")
+	new_filename=$(echo "$DIR_PATH/$filename")
 	if [[ -f "$new_filename" && ! -L "new_filename" ]]; then
-		#mv "$new_filename" "$new_filename.old"
+		mv -nu "$new_filename" "$new_filename.old"
 		echo "File $new_filename renamed to $new_filename.old"
 	fi
 
-	ln -s -f "$CUR_PATH/kernel/$filename" "$new_filename"
-	echo "Created symlink from $CUR_PATH/kernel/$filename to $new_filename"
+	ln -s -f "$CUR_PATH/src/$filename" "$new_filename"
+	echo "Created symlink from $CUR_PATH/src/$filename to $new_filename"
 done <<< "$NEW_FILES"
